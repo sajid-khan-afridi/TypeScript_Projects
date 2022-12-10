0a5a314573fe58b1825653c4da84df0125e0f5db
sajidkhanafridi <uetianafridi@gmail.com>
2022-12-10 09:39:22 +0500

    Create index.js

---
+++ atm/arrayUpdating/index.js
@@ -0,0 +1,9 @@
+const Array_obj = [
+  { id: 0, name: "David" },
+
+  { id: 1, name: "John" },
+];
+upd_obj = Array_obj.findIndex((obj) => obj.id == 0);
+console.log("Before Object Updation: ", Array_obj[upd_obj]);
+Array_obj[upd_obj].name = "Harry";
+console.log("After Object Updation: ", Array_obj[upd_obj]);
