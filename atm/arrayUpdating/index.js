c16722a8179eec5b55b820de7b493ebfa867bd51
sajidkhanafridi <uetianafridi@gmail.com>
2022-12-10 10:25:58 +0500

    Update index.js

--- atm/arrayUpdating/index.js
+++ atm/arrayUpdating/index.js
@@ -5,5 +5,6 @@ const Array_obj = [
 ];
 upd_obj = Array_obj.findIndex((obj) => obj.id == 0);
 console.log("Before Object Updation: ", Array_obj[upd_obj]);
+
 Array_obj[upd_obj].name = "Harry";
 console.log("After Object Updation: ", Array_obj[upd_obj]);
