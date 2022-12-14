5bfdae763050e0122f7c8bfc629dfe8b5131c5e0
sajid-khan-afridi <69663016+sajid-khan-afridi@users.noreply.github.com>
2022-12-14 19:59:56 +0500

    todo app complete

--- todo_app/2/index.js
+++ todo_app/2/index.js
@@ -26,66 +26,63 @@ import inquirer from "inquirer";
         });
     }
     displayTask();
-    // let condition = true;
-    // while (condition) {
-    let answer = await inquirer.prompt({
-        name: "option",
-        type: "list",
-        choices: ["Add New Task", "Delete Task"],
-        message: "What action do you want to perform",
-    });
-    switch (answer.option) {
-        case "Add New Task":
-            addTask();
-            break;
-        case "Delete Task":
-            deleteTask();
-            break;
-    }
-    // const deleteTodo = (id: number) => {
-    //   const index = tasks.findIndex((obj) => obj.id === id);
-    //   tasks.splice(index, 1);
-    // };
-    // deleteTodo(2);
-    // console.log(tasks);
-    async function addTask() {
-        let add = await inquirer.prompt([
-            {
-                name: "id",
-                message: "Enter the task id",
-                type: "input",
-            },
-            {
-                name: "name",
-                message: "Enter the task name",
-                type: "input",
-            },
-            {
-                name: "description",
-                message: "Enter the task description",
-                type: "input",
-            },
-        ]);
-        tasks.push(add);
-        displayTask();
-    }
-    async function deleteTask() {
-        let deleteTask = await inquirer.prompt({
-            name: "task",
-            type: "input",
-            message: "Enter the Task Name to delete",
+    let condition = true;
+    while (condition) {
+        let answer = await inquirer.prompt({
+            name: "option",
+            type: "list",
+            choices: ["Add New Task", "Delete Task"],
+            message: "What action do you want to perform",
         });
-        const index = tasks.findIndex((obj) => obj.name === deleteTask.task);
-        tasks.splice(index, 1);
-        displayTask();
+        switch (answer.option) {
+            case "Add New Task":
+                let add = await inquirer.prompt([
+                    {
+                        name: "id",
+                        message: "Enter the task id",
+                        type: "input",
+                    },
+                    {
+                        name: "name",
+                        message: "Enter the task name",
+                        type: "input",
+                    },
+                    {
+                        name: "description",
+                        message: "Enter the task description",
+                        type: "input",
+                    },
+                ]);
+                tasks.push(add);
+                displayTask();
+                break;
+            case "Delete Task":
+                let deleteTask = await inquirer.prompt({
+                    name: "task",
+                    type: "input",
+                    message: "Enter the Task Name to delete",
+                });
+                const index = tasks.findIndex((obj) => obj.name === deleteTask.task);
+                tasks.splice(index, 1);
+                displayTask();
+                break;
+        }
+        // const deleteTodo = (id: number) => {
+        //   const index = tasks.findIndex((obj) => obj.id === id);
+        //   tasks.splice(index, 1);
+        // };
+        // deleteTodo(2);
+        // console.log(tasks);
+        // async function addTask() {}
+        // async function deleteTask() {}
+        // async function again() {
+        let cond = await inquirer.prompt({
+            name: "yesNo",
+            type: "confirm",
+            message: "Do you want to continue or not?",
+        });
+        console.log(cond.yesNo);
+        if (cond.yesNo === "No")
+            condition = false;
     }
-    // async function again() {
-    //   let cond = await inquirer.prompt({
-    //     name: "yesNo",
-    //     type: "confirm",
-    //     message: "Do you want to continue or not?",
-    //   });
-    //   if (cond.yesNo === "No") condition = false;
-    // }
-    // }
 })();
--- todo_app/2/index.ts
+++ todo_app/2/index.ts
@@ -27,69 +27,67 @@ import inquirer from "inquirer";
   }
   displayTask();
 
-  // let condition = true;
-  // while (condition) {
-  let answer: { option: "Add New Task" | "Delete Task" } =
-    await inquirer.prompt({
-      name: "option",
-      type: "list",
-      choices: ["Add New Task", "Delete Task"],
-      message: "What action do you want to perform",
-    });
-  switch (answer.option) {
-    case "Add New Task":
-      addTask();
-      break;
-    case "Delete Task":
-      deleteTask();
-      break;
-  }
-  // const deleteTodo = (id: number) => {
-  //   const index = tasks.findIndex((obj) => obj.id === id);
-  //   tasks.splice(index, 1);
-  // };
-  // deleteTodo(2);
-  // console.log(tasks);
-  async function addTask() {
-    let add: { id: number; name: string; description: string } =
-      await inquirer.prompt([
-        {
-          name: "id",
-          message: "Enter the task id",
+  let condition = true;
+  while (condition) {
+    let answer: { option: "Add New Task" | "Delete Task" } =
+      await inquirer.prompt({
+        name: "option",
+        type: "list",
+        choices: ["Add New Task", "Delete Task"],
+        message: "What action do you want to perform",
+      });
+    switch (answer.option) {
+      case "Add New Task":
+        let add: { id: number; name: string; description: string } =
+          await inquirer.prompt([
+            {
+              name: "id",
+              message: "Enter the task id",
+              type: "input",
+            },
+            {
+              name: "name",
+              message: "Enter the task name",
+              type: "input",
+            },
+            {
+              name: "description",
+              message: "Enter the task description",
+              type: "input",
+            },
+          ]);
+        tasks.push(add);
+        displayTask();
+        break;
+      case "Delete Task":
+        let deleteTask = await inquirer.prompt({
+          name: "task",
           type: "input",
-        },
-        {
-          name: "name",
-          message: "Enter the task name",
-          type: "input",
-        },
-        {
-          name: "description",
-          message: "Enter the task description",
-          type: "input",
-        },
-      ]);
-    tasks.push(add);
-    displayTask();
-  }
-  async function deleteTask() {
-    let deleteTask = await inquirer.prompt({
-      name: "task",
-      type: "input",
-      message: "Enter the Task Name to delete",
-    });
-    const index = tasks.findIndex((obj) => obj.name === deleteTask.task);
-    tasks.splice(index, 1);
-    displayTask();
-  }
+          message: "Enter the Task Name to delete",
+        });
+        const index = tasks.findIndex((obj) => obj.name === deleteTask.task);
+        tasks.splice(index, 1);
+        displayTask();
 
-  // async function again() {
-  //   let cond = await inquirer.prompt({
-  //     name: "yesNo",
-  //     type: "confirm",
-  //     message: "Do you want to continue or not?",
-  //   });
-  //   if (cond.yesNo === "No") condition = false;
-  // }
-  // }
+        break;
+    }
+    // const deleteTodo = (id: number) => {
+    //   const index = tasks.findIndex((obj) => obj.id === id);
+    //   tasks.splice(index, 1);
+    // };
+    // deleteTodo(2);
+    // console.log(tasks);
+    // async function addTask() {}
+
+    // async function deleteTask() {}
+
+    // async function again() {
+    let cond = await inquirer.prompt({
+      name: "yesNo",
+      type: "confirm",
+      message: "Do you want to continue or not?",
+    });
+    console.log(cond.yesNo);
+    if (cond.yesNo === false) condition = false;
+  }
 })();
