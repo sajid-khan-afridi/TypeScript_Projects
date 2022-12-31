da890ca79a40b66b6a198bd3f71a029d8a0e689e
sajid-khan-afridi <69663016+sajid-khan-afridi@users.noreply.github.com>
2022-12-31 13:01:51 +0500

    numberOfWords

--- project05_word_counter/1/index.js
+++ project05_word_counter/1/index.js
@@ -1,13 +1,11 @@
 import inquirer from "inquirer";
-
 let answer = await inquirer.prompt([
-  {
-    name: "para",
-    type: "input",
-    message: "Please Enter Your Paragraph",
-  },
+    {
+        name: "para",
+        type: "input",
+        message: "Please Enter Your Paragraph",
+    },
 ]);
 let { para } = answer;
 let numberOfWords = para.split(" ").length;
-
 console.log(numberOfWords);
