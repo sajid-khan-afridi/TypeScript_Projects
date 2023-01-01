7b8755f884d2d5db7d9b8bd3b34f135d343d6646
sajid-khan-afridi <69663016+sajid-khan-afridi@users.noreply.github.com>
2023-01-01 10:12:19 +0500

    Update index.ts

--- project06_student_management_system/prac/index.ts
+++ project06_student_management_system/prac/index.ts
@@ -1,10 +1,16 @@
-class Student {
+interface Person {
+  firstName: string;
+  lastName: string;
+}
+class Student implements Person {
   id: number;
-  name: string;
+  firstName: string;
+  lastName: string;
   courses: Course[] = [];
-  constructor(id: number, name: string) {
+  constructor(id: number, firstName: string, lastName: string) {
     this.id = id;
-    this.name = name;
+    this.firstName = firstName;
+    this.lastName = lastName;
   }
   registerForCourse(course: Course) {
     this.courses.push(course);
@@ -22,7 +28,30 @@ class Course {
   // addInstructor(instructor: string) {
   //   this.instructor = instructor;
   // }
-  addStudent(student: Student) {
-    this.student.push(student);
+  // addStudent(student: Student) {
+  //   this.student.push(student);
+  // }
+}
+
+class StudentManagementSystem {
+  students: Student[] = [];
+  courses: Course[] = [];
+  enroll(student: Student, course: Course): void {
+    // Enroll the student in the course
+  }
+
+  viewBalance(student: Student): number {
+    // View the balance of the student
+  }
+
+  payTuition(student: Student, amount: number): void {
+    // Pay the tuition fees for the student
+  }
+
+  showStatus(student: Student): void {
+    // Show the status of the student, including name, id, courses, and balance
   }
 }
+const stuObj = new Student(1, "sajid", "afridi");
+stuObj.registerForCourse;
+const crsObj = new Course("Metaverse", "Sir Zia");
