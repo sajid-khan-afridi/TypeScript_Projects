76e2e2b6d24a23e0ff7096ba75ce64e49ee709eb
sajid-khan-afridi <69663016+sajid-khan-afridi@users.noreply.github.com>
2023-01-01 15:43:15 +0500

    Update index.ts

--- helpdesk/1jan/index.ts
+++ helpdesk/1jan/index.ts
@@ -63,3 +63,33 @@
 // console.log(obj.length);
 
 //                            //Index Signatures
+class MyClass {
+  [s: string]: boolean | ((s: string) => boolean);
+  // sum = true;
+
+  check(s: string) {
+    return this[s] as boolean;
+  }
+  // check2(s: string) {
+  //   return this.sum;
+  // }
+}
+
+//                    //Class Heritage
+interface Pingable {
+  ping(): void;
+}
+
+class Sonar implements Pingable {
+  ping() {
+    console.log("ping!");
+  }
+}
+
+class Ball implements Pingable {
+  // Class 'Ball' incorrectly implements interface 'Pingable'.
+  //   Property 'ping' is missing in type 'Ball' but required in type 'Pingable'.
+  pong() {
+    console.log("pong!");
+  }
+}
