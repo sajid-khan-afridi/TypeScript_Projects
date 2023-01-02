7b9be62d798f760a17790235470c3a119ec261ab
Sajid Khan Afridi <69663016+sajid-khan-afridi@users.noreply.github.com>
2023-01-02 07:33:00 +0500

    Update index.ts

--- project06_student_management_system/prac/index.ts
+++ project06_student_management_system/prac/index.ts
@@ -6,36 +6,53 @@ class Student implements Person {
   id: number;
   firstName: string;
   lastName: string;
+  balance: number;
   courses: Course[] = [];
-  constructor(id: number, firstName: string, lastName: string) {
+  constructor(
+    id: number,
+    firstName: string,
+    lastName: string,
+    balance: number
+  ) {
     this.id = id;
     this.firstName = firstName;
     this.lastName = lastName;
+    this.balance = balance;
   }
   registerForCourse(course: Course) {
     this.courses.push(course);
   }
 }
 
+interface CourseInterface {}
+
 class Course {
   name: string;
   student: Student[] = [];
   instructor!: string;
-  constructor(name: string, instructor: string) {
+  cost: number;
+  constructor(name: string, instructor: string, cost: number) {
     this.name = name;
     this.instructor = instructor;
+    this.cost = cost;
   }
   // addInstructor(instructor: string) {
   //   this.instructor = instructor;
   // }
-  // addStudent(student: Student) {
-  //   this.student.push(student);
-  // }
+  addStudent(student: Student) {
+    this.student.push(student);
+  }
 }
 
 class StudentManagementSystem {
   students: Student[] = [];
   courses: Course[] = [];
+  // balance: number;
+
+  constructor(student: Student, course: Course) {
+    this.students.push(student);
+    this.courses.push(course);
+  }
   enroll(student: Student, course: Course): void {
     // Enroll the student in the course
   }
@@ -52,6 +69,9 @@ class StudentManagementSystem {
     // Show the status of the student, including name, id, courses, and balance
   }
 }
-const stuObj = new Student(1, "sajid", "afridi");
-stuObj.registerForCourse;
-const crsObj = new Course("Metaverse", "Sir Zia");
+const stuObj1 = new Student(1, "sajid", "afridi", 50000);
+const crsObj1 = new Course("Metaverse", "Sir Zia", 12000);
+const crsObj2 = new Course("Block Chain", "Sir Daniyal", 12000);
+stuObj1.registerForCourse(crsObj1);
+stuObj1.registerForCourse(crsObj2);
+crsObj1.addStudent(stuObj1);
